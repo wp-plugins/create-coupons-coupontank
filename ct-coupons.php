a6d52649cc68598a767fc4fe393f1a3de4ab6e61
coupontank <coupontank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-27 07:43:43 +0000

    Upgraded to coupontank 2.0
    git-svn-id: https://plugins.svn.wordpress.org/create-coupons-coupontank/trunk@389651 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ct-coupons.php
+++ ct-coupons.php
@@ -3,7 +3,7 @@
 Plugin Name: CoupontankCoupons
 Plugin URI: http://www.coupontank.com/signup/
 Description: Retrieves a list of coupons that you design with coupontank.com
-Version: 1.0
+Version: 2.0
 Author: CouponTank.com
 Author URI: http://www.coupontank.com/
 */
@@ -12,73 +12,23 @@ Author URI: http://www.coupontank.com/
 // account.  Just go to www.coupontank.com
 global $coupontank;
 $coupontank = array(
-  'api_id' => 'your_api_id_goes_here',
-  'api_pass' => 'your_api_pass_goes_here',
-  'theme' => 'red'
+  'subdomain' => 'sample',
 );
 
-class ct_my_coupons extends WP_Widget{
-
-function ct_my_coupons() {
-    parent::WP_Widget(false, $name = 'Coupontank Coupons');
-}
-
-function widget($args, $instance) {
+function ct_draw_tab() {
     global $coupontank;
-    //$output = wp_cache_get('coupontank_coupons');
-    extract($args);
-
-    $output = false;	
-    echo $before_widget;
-    if ( false === $output ) {
-        $ch = curl_init('http://www.coupontank.com/app/Api/loadcoupons/api_id/'.$coupontank['api_id'].'/api_pass/'.$coupontank['api_pass']);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $coupons = unserialize(curl_exec($ch));
-	curl_close($ch);
-        $output = '';
-        if ( !$coupons['error'] ) {
-            $idx = 0;
-	    $output = '<div class="ctModule style">'.$before_title.'<h1> Special Offers</h1>'.$after_title.'<ul>' . "\n";
-            foreach ($coupons['coupons'] as $coupon) {
-	        $idx++;
-                $output .= '<li '.((count($coupons['coupons']) == $idx)?'class="last"':'').'>' . "\n";
-		$output .= '<a href="'.$coupon['url'].'">'.str_replace('<br/>', ' ', $coupon['title_name']).'</a>' . "\n";
-                $output .= '<span class="desc">'.str_replace('<br/>', ' ', $coupon['offer_name']).'</span>';
-		if (!empty($coupon['promo_code'])) {
-		    $output .= '<span class="code">Use Coupon Code: '.$coupon['promo_code'].'</span>';
-		}
-                $output .= '</li>' . "\n";
-
-            }
-            $output .= '</ul></div>';
-	    print $output;
-            //wp_cache_set('coupontank_coupons', $output);
-        } else {
-            $output .= 'Invalid API Credentials.';
-        }
-    }
-    echo $after_widget;
+?>
+<script type="text/javascript" src="http://www.coupontank.com/js/jquery.js"></script> 
+<script type="text/javascript" src="http://<?=$coupontank['subdomain']?>.coupontank.com/Tab/js.js"></script> 
+<link href="http://<?=$coupontank['subdomain']?>.coupontank.com/Tab/css.css" rel="stylesheet" type="text/css" /> 
+<div class="coupon-panel"> 
+  <img src="http://<?=$coupontank['subdomain']?>.coupontank.com/Tab/image.png" id="coupon-panel-tab"/> 
+  <iframe width="490" height="420" rel="http://<?=$coupontank['subdomain']?>.coupontank.com/tab" scrollbar="no" marginwidth="0" marginheight="0" hspace="0" align="middle" frameborder="0"></iframe> 
+</div> 
+<?
 }
 
-function update($new_instance, $old_instance) {
-  return $new_instance;
-}
-
-
-}
-
-function ct_coupon_css() {
-    $siteurl=get_option('siteurl');
-    global $coupontank;
-    $theme = $coupontank['theme'];
-    ?>
-    <link href='<?php echo $siteurl; ?>/wp-content/plugins/create-coupons-coupontank/themes/<?php echo $theme; ?>/style.css' rel="stylesheet" type="text/css" />
-    <?
-}
-
-
-//wp_register_sidebar_widget('ct_widget_coupon', 'CouponTank Coupons', 'ct_my_coupons');
-add_action('wp_head','ct_coupon_css',11,0);
-add_action('widgets_init', create_function('', 'return register_widget("ct_my_coupons");'));	
+add_action('wp_footer', 'ct_draw_tab');	
 
 ?>
+
