971a1b4c19c3c0491818f91293a96b3a64a9f89a
coupontank <coupontank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-14 03:57:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/create-coupons-coupontank/trunk@437841 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ct-coupons.php
+++ ct-coupons.php
@@ -3,7 +3,7 @@
 Plugin Name: CoupontankCoupons
 Plugin URI: http://www.coupontank.com/signup/
 Description: Retrieves a list of coupons that you design with coupontank.com
-Version: 2.0.5
+Version: 2.0.6
 Author: CouponTank.com
 Author URI: http://www.coupontank.com/
 */
@@ -29,7 +29,7 @@ function ct_draw_tab() {
    }
 </script> 
 <script type="text/javascript" src="https://<?=$coupontank['subdomain']?>.mycoupontank.com/Tab/js.js"></script> 
-<link href="httpis://<?=$coupontank['subdomain']?>.mycoupontank.com/Tab/css.css" rel="stylesheet" type="text/css" /> 
+<link href="https://<?=$coupontank['subdomain']?>.mycoupontank.com/Tab/css.css" rel="stylesheet" type="text/css" /> 
 <div class="coupon-panel"> 
   <a href="https://<?=$coupontank['subdomain']?>.mycoupontank.com" target="_blank"><img src="http://<?=$coupontank['subdomain']?>.mycoupontank.com/Tab/image.png" id="coupon-panel-tab"/></a>
   <iframe width="490" height="420" rel="https://<?=$coupontank['subdomain']?>.mycoupontank.com/plugin?wordpress=1" scrollbar="no" marginwidth="0" marginheight="0" hspace="0" align="middle" frameborder="0"></iframe> 
