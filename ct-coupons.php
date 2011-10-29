415cec256f12cded3c59f00991bb4b50b3b16586
coupontank <coupontank@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-29 04:32:08 +0000

    git-svn-id: https://plugins.svn.wordpress.org/create-coupons-coupontank/trunk@456943 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ct-coupons.php
+++ ct-coupons.php
@@ -3,7 +3,7 @@
 Plugin Name: CoupontankCoupons
 Plugin URI: http://www.coupontank.com/signup/
 Description: Retrieves a list of coupons that you design with coupontank.com
-Version: 2.0.6
+Version: 2.1.0
 Author: CouponTank.com
 Author URI: http://www.coupontank.com/
 */
@@ -18,21 +18,12 @@ $coupontank = array(
 function ct_draw_tab() {
     global $coupontank;
 ?>
-<script type="text/javascript">
-   if (typeof jQuery == 'undefined') { 
-     var head = document.getElementsByTagName("head")[0];
-     script = document.createElement('script');
-     script.id = 'jQuery';
-     script.type = 'text/javascript';
-     script.src = 'https://www.mycoupontank.com/js/jquery.js';
-     head.appendChild(script);
-   }
-</script> 
-<script type="text/javascript" src="https://<?=$coupontank['subdomain']?>.mycoupontank.com/Tab/js.js"></script> 
-<link href="https://<?=$coupontank['subdomain']?>.mycoupontank.com/Tab/css.css" rel="stylesheet" type="text/css" /> 
+<script type="text/javascript" src="https://www.mycoupontank.com/js/jquery.js"></script>
+<script type="text/javascript" src="https://<?=$coupontank['subdomain']?>.mycoupontank.com/Wordpress/js.js"></script> 
+<link href="https://<?=$coupontank['subdomain']?>.mycoupontank.com/Wordpress/css.css" rel="stylesheet" type="text/css" /> 
 <div class="coupon-panel"> 
-  <a href="https://<?=$coupontank['subdomain']?>.mycoupontank.com" target="_blank"><img src="http://<?=$coupontank['subdomain']?>.mycoupontank.com/Tab/image.png" id="coupon-panel-tab"/></a>
-  <iframe width="490" height="420" rel="https://<?=$coupontank['subdomain']?>.mycoupontank.com/plugin?wordpress=1" scrollbar="no" marginwidth="0" marginheight="0" hspace="0" align="middle" frameborder="0"></iframe> 
+  <a href="https://<?=$coupontank['subdomain']?>.mycoupontank.com" target="_blank"><img src="http://<?=$coupontank['subdomain']?>.mycoupontank.com/Wordpress/image.png" id="coupon-panel-tab"/></a>
+  <iframe width="490" height="420" rel="https://<?=$coupontank['subdomain']?>.mycoupontank.com/Wordpress/canvas" scrollbar="no" marginwidth="0" marginheight="0" hspace="0" align="middle" frameborder="0"></iframe> 
 </div> 
 <?
 }
